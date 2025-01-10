63a08d19d658f3bc08e71e6fba45f258c9bfbfe3
Usman <usmantufail837@gmail.com>
2025-01-10 16:02:06 +0500

    create testimonial component and add testimon ial section in home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner/Banner"
 import Memories from "../components/Memories/Memories"
+import Testimonials from "../components/Testimonials/Testimonials"
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Memories />
+      <Testimonials/>
     </>
   )
 }
