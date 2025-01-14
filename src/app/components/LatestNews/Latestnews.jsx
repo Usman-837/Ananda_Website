0965827d8971bc1c9eb5a26755c1367558e677f0
Usman <usmantufail837@gmail.com>
2025-01-14 13:08:43 +0500

    add responsive functionality in latestnews component to make it responsive

--- src/app/components/LatestNews/Latestnews.jsx
+++ src/app/components/LatestNews/Latestnews.jsx
@@ -36,7 +36,7 @@ const Latestnews = () => {
                     <h1 className='Vujahday text-[#939580] text-4xl'>Latest News</h1>
                     <p className='text-[40px] uppercase'>Read Our Wedding News</p>
                 </div>
-                <div className='flex gap-x-10'>
+                <div className='flex gap-x-10 max-lg:flex-col  max-lg:gap-y-10'>
                     <div className='space-y-4'>
                         <Image src={newsimg1} alt='latestnews' />
                         <div className='flex gap-4'>
@@ -45,19 +45,19 @@ const Latestnews = () => {
                                     <li className="w-2 h-2 bg-black rounded-full text-transparent font-semibold font-outline-2"></li>
                                     <li className='text-[50px] text-transparent font-semibold font-outline-2'>25</li>
                                 </ul>
-                                <p className='text-[#939580]'>June</p>
+                                <p className='text-[#939580] text-end'>June</p>
                             </div>
-                            <p className='text-4xl'>Photography is the important part
+                            <p className='text-4xl max-sm:text-2xl'>Photography is the important part
                                 of wedding.</p>
                         </div>
                     </div>
                     <div className='space-y-7'>
                         {
                             NewsData.map((data) => (
-                                <div key={data.id} className='flex gap-x-6'>
+                                <div key={data.id} className='flex gap-x-6 max-sm:flex-col max-sm:items-center max-sm:justify-center max-sm:text-center max-sm:gap-y-5'>
                                     <Image src={data.img} alt='latestnews' />
                                     <div className='space-y-4'>
-                                        <p className='text-[28px] leading-9'>{data.title}</p>
+                                        <p className='text-[28px] max-sm:text-xl leading-9'>{data.title}</p>
                                         <p className='text-[#939580] text-lg pl-5'>{data.date}</p>
                                     </div>
                                 </div>
