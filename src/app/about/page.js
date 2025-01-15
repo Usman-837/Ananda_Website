8a51640e4ff4b2f5138c05bfa2ea3123e569b7d4
Usman <usmantufail837@gmail.com>
2025-01-15 15:16:09 +0500

    create welcome component and add welcome section in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,6 +3,7 @@ import Navbar from '../components/About/Navbar/Navbar'
 import Banner from '../components/About/Banner/Banner'
 import AboutUs from '../components/About/AboutUs/AboutUs'
 import Rating from '../components/About/Rating/Rating'
+import Welcome from '../components/About/Welcome/Welcome'
 
 const About = () => {
   return (
@@ -11,6 +12,7 @@ const About = () => {
       <Banner />
       <AboutUs/>
       <Rating/>
+      <Welcome/>
     </div>
   )
 }
--- src/app/components/About/Rating/Rating.jsx
+++ src/app/components/About/Rating/Rating.jsx
@@ -7,51 +7,9 @@ import Ratingimg3 from '../../../../../public/About/Rating/image3.png'
 
 const Rating = () => {
 
-    const RatingData = [
-        {
-            id: 1,
-            num: '1,360+',
-            title: 'Happy Couples',
-            img: Ratingimg1
-        },
-        {
-            id: 2,
-            num: '503+',
-            title: 'Decoration',
-            img: Ratingimg2
-        },
-        {
-            id: 3,
-            num: '1530+',
-            title: 'Wedding',
-            img: Ratingimg3
-        },
-        {
-            id: 4,
-            num: '306',
-            title: 'Location'
-        }
-    ]
-
-
     return (
         <div className='bg-[#FAF6F2] py-28'>
             <div className='container'>
-                {/* <div className='flex justify-between items-center'>
-                {
-                    RatingData.map((data) => (
-                        <div key={data.id} className='flex justify-between'>
-                            <div className='space-y-6'>
-                                <p className='text-6xl'>{data.num}</p>
-                                <p className='text-[#939580] text-[25px]'>{data.title}</p>
-                            </div>
-                            <div>
-                                <Image src={data.img} alt='ratingimage' />
-                            </div>
-                        </div>
-                    ))
-                }
-                </div> */}
                 <div className='grid grid-cols-4 max-xl:grid-cols-3 max-laptop:grid-cols-2 max-sm:grid-cols-1 max-sm:items-center max-sm:justify-center gap-12'>
                     <div className='flex gap-x-12 max-sm:items-center max-sm:justify-center'>
                         <div className='space-y-6 max-sm:space-y-3'>
