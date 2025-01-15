daacdc859643672bc5dcef9c4ef80df650d313a0
Usman <usmantufail837@gmail.com>
2025-01-15 16:19:08 +0500

    add wedding story, partners and contact section in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,6 +5,9 @@ import AboutUs from '../components/About/AboutUs/AboutUs'
 import Rating from '../components/About/Rating/Rating'
 import Welcome from '../components/About/Welcome/Welcome'
 import Teammembers from '../components/About/TeamMembers/Teammembers'
+import WeddingStory from '../components/Home/WeddingStory/WeddingStory'
+import Partners from '../components/Home/Partners/Partners'
+import Contact from '../components/Home/Contact/Contact'
 
 const About = () => {
   return (
@@ -15,6 +18,9 @@ const About = () => {
       <Rating/>
       <Welcome/>
       <Teammembers/>
+      <WeddingStory/>
+      <Partners/>
+      <Contact/>
     </div>
   )
 }
