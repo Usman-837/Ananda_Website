d101c4c91c0de79e16ec28084a07a5ad8cf21eb9
Usman <usmantufail837@gmail.com>
2025-01-15 15:26:45 +0500

    create teammember component and add headings init

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,7 @@ import Banner from '../components/About/Banner/Banner'
 import AboutUs from '../components/About/AboutUs/AboutUs'
 import Rating from '../components/About/Rating/Rating'
 import Welcome from '../components/About/Welcome/Welcome'
+import Teammembers from '../components/About/TeamMembers/Teammembers'
 
 const About = () => {
   return (
@@ -13,6 +14,7 @@ const About = () => {
       <AboutUs/>
       <Rating/>
       <Welcome/>
+      <Teammembers/>
     </div>
   )
 }
---
+++ src/app/components/About/TeamMembers/Teammembers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Teammembers = () => {
+    return (
+        <div className='bg-[#FAF6F2] py-28'>
+            <div className='container'>
+                <div>
+                    <h1 className='text-[#939580] text-[35px] text-center'>Our Organizers</h1>
+                    <p className='uppercase text-center pt-3 text-[40px]'>Awesome Team Member</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Teammembers
