4906d00bca195fdcf7dd619d498bccbff42f100b
Usman <usmantufail837@gmail.com>
2025-01-15 15:50:54 +0500

    add team members in teammembers section

--- src/app/components/About/TeamMembers/Teammembers.jsx
+++ src/app/components/About/TeamMembers/Teammembers.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
+import Image from 'next/image'
+import memberimg1 from '../../../../../public/About/TeamMember/image1.png'
+import memberimg2 from '../../../../../public/About/TeamMember/image2.png'
+import memberimg3 from '../../../../../public/About/TeamMember/image3.png'
+import memberimg4 from '../../../../../public/About/TeamMember/image4.png'
 
 const Teammembers = () => {
+
+    const TeamMemberData = [
+        {
+            id: 1,
+            name: 'Margarate Bennet',
+            title: 'Dress Designer',
+            img: memberimg1,
+        },
+        {
+            id: 2,
+            name: 'Margarate Bennet',
+            title: 'Dress Designer',
+            img: memberimg2,
+        },
+        {
+            id: 3,
+            name: 'Henry D’suza',
+            title: 'Assistant',
+            img: memberimg3,
+        },
+        {
+            id: 4,
+            name: 'Jenefer Abram',
+            title: 'Lead Assistant',
+            img: memberimg4,
+        },
+    ]
+
+
     return (
         <div className='bg-[#FAF6F2] py-28'>
             <div className='container'>
                 <div>
                     <h1 className='text-[#939580] text-[35px] text-center'>Our Organizers</h1>
                     <p className='uppercase text-center pt-3 text-[40px]'>Awesome Team Member</p>
+                </div>
+                <div className='pt-14 grid grid-cols-4'>
+                    {
+                        TeamMemberData.map((data) => (
+                            <div key={data.id} className='flex flex-col items-center justify-center gap-y-3'>
+                                <Image src={data.img} alt='teammember' className='rounded-full' />
+                                <p className='text-[25px]'>{data.name}</p>
+                                <p className='text-lg text-[#939580]'>{data.title}</p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
