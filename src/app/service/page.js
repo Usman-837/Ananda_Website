56d18cd7595634d900c93ce83cdea3ed7e7064aa
Usman <usmantufail837@gmail.com>
2025-01-16 09:53:21 +0500

    create Banner componnet and add banner section in service page

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/About/Navbar/Navbar'
+import Banner from '../components/Service/Banner/Banner'
 
 const Service = () => {
   return (
     <div>
       <Navbar/>
+      <Banner/>
     </div>
   )
 }
