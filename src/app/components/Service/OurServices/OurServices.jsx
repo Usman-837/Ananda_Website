43e860123596bd78491cbb77912c38f1571d8c76
Usman <usmantufail837@gmail.com>
2025-01-16 10:05:19 +0500

    create ourservices component and add our services section in service page

---
+++ src/app/components/Service/OurServices/OurServices.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OurServices = () => {
+  return (
+    <div className='py-28'>
+        <div className='container'>
+            <div>
+              <h1 className='text-center text-[35px] text-[#939580]'>Our Services</h1>
+              <p className='text-[40px] text-center pt-3 uppercase'>WHAT WE OFFER FOR YOU</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OurServices
--- src/app/service/page.js
+++ src/app/service/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/About/Navbar/Navbar'
 import Banner from '../components/Service/Banner/Banner'
+import OurServices from '../components/Service/OurServices/OurServices'
 
 const Service = () => {
   return (
-    <div>
+    <div className='bg-[#FAF6F2]'>
       <Navbar/>
       <Banner/>
+      <OurServices/>
     </div>
   )
 }
