932f5bea3d6ce7278c15607e5c15596ab4ffa268
Usman <usmantufail837@gmail.com>
2025-01-16 09:48:18 +0500

    create service component and add navbar init and update service menu in navbar of home and about page

---
+++ src/app/service/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/About/Navbar/Navbar'
+
+const Service = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Service
