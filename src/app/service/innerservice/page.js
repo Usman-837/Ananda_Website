d14e3c4b59f4c28e58d330d202e708690902055c
Usman <usmantufail837@gmail.com>
2025-01-16 11:30:54 +0500

    create innerservice and add navbar and banner section init

---
+++ src/app/service/innerservice/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/app/components/About/Navbar/Navbar'
+import Banner from '@/app/components/Service/Banner/Banner'
+
+const InnerService = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default InnerService
