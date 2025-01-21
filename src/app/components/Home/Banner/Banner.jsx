20b7ab7290d4099802498f48918a03c8c28f9be4
Usman <usmantufail837@gmail.com>
2025-01-21 15:04:24 +0500

    update banner of home page

--- src/app/components/Home/Banner/Banner.jsx
+++ src/app/components/Home/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import banner from '../../../../../public/Home/Banner/Banner.jpg'
 const Banner = () => {
   return (
     <div
-      className="w-full h-svh max-sm:h-[200px] bg-cover bg-center"
+      className="w-full h-svh bg-cover bg-center mt-[-118px] max-sm:mt-[-265px]"
       style={{
         backgroundImage: `url(${banner.src})`, 
       }}
