d3a2ef2c59cce9b5b907b2827173735587241f4e
Usman <usmantufail837@gmail.com>
2025-01-21 16:18:20 +0500

    add gsap animation in banner of home page

--- src/app/components/Home/Banner/Banner.jsx
+++ src/app/components/Home/Banner/Banner.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import banner from '../../../../../public/Home/Banner/Banner.jpg'
+'use client'
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import banner from '../../../../../public/Home/Banner/Banner.jpg';
 
 const Banner = () => {
+  const bannerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      bannerRef.current,
+      { y: 100, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1.5, ease: 'power2.out' }
+    );
+  }, []);
+  
+
   return (
     <div
+      ref={bannerRef}
       className="w-full h-svh bg-cover bg-center mt-[-118px] max-sm:mt-[-265px]"
       style={{
-        backgroundImage: `url(${banner.src})`, 
+        backgroundImage: `url(${banner.src})`,
       }}
-    >
+    ></div>
+  );
+};
 
-    </div>
-  )
-}
+export default Banner;
 
-export default Banner
+
