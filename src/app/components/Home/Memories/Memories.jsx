666ff677f69f9be5daa444173500a5fec1ae6d52
Usman <usmantufail837@gmail.com>
2025-01-31 11:14:19 +0500

    add more cards in momories section sliders in home page

--- src/app/components/Home/Memories/Memories.jsx
+++ src/app/components/Home/Memories/Memories.jsx
@@ -8,51 +8,80 @@ import cardimage4 from '../../../../../public/Home/Memories/image4.jpg'
 import cardimage5 from '../../../../../public/Home/Memories/image5.png'
 import cardimage6 from '../../../../../public/Home/Memories/image9.png'
 import cardimage7 from '../../../../../public/Home/Memories/image10.png'
+import cardimage8 from '../../../../../public/Home/Memories/image11.png'
+import cardimage9 from '../../../../../public/Home/Memories/image12.png'
+import cardimage10 from '../../../../../public/Home/Memories/image13.png'
 import Memoryimg from '../../../../../public/Home/Memories/image6.png'
 import Memoryimg2 from '../../../../../public/Home/Memories/image8.png'
 import logo from '../../../../../public/Home/Logo/logo.png'
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import Link from 'next/link'
 
 const Memories = () => {
 
     const cardData = [
-        
+
         {
             id: 1,
             name: 'Style by Ananda',
             image: cardimage6,
+            route: 'services/1'
         },
         {
             id: 2,
             name: 'Jewelry by Ananda',
             image: cardimage7,
+            route: 'services/2'
         },
         {
             id: 3,
             name: 'Wedding Dress',
             image: cardimage1,
+            route: 'services/3'
         },
         {
             id: 4,
-            name: 'Wedding Regist',
+            name: 'Event Planning',
             image: cardimage2,
+            route: 'services/4'
         },
         {
             id: 5,
             name: 'Photography',
             image: cardimage3,
+            route: 'services/5'
         },
         {
             id: 6,
             name: 'Cake Design',
             image: cardimage4,
+            route: 'services/6'
         },
         {
             id: 7,
-            name: 'Event Planning',
+            name: 'Venu Selection',
             image: cardimage5,
+            route: 'services/7'
+        },
+        {
+            id: 8,
+            name: 'Decoration',
+            image: cardimage8,
+            route: 'services/8'
+        },
+        {
+            id: 9,
+            name: 'Cetering',
+            image: cardimage9,
+            route: 'services/9'
+        },
+        {
+            id: 10,
+            name: 'Entertainment',
+            image: cardimage10,
+            route: 'services/10'
         },
     ]
 
@@ -128,7 +157,7 @@ const Memories = () => {
                             </div>
                             <div className='flex flex-col items-center justify-center text-center gap-y-5'>
                                 <h1 className='Vujahday text-sixtyfive max-sm:text-4xl leading-sixtyeight tracking-0.5'>special moments</h1>
-                                <p className='Roboto text-DimGray text-lg max-sm:text-sm leading-twentyseven tracking-0.5'>Showcase the bridal party and groomsmen with photos and<br/> brief bios. Share heartfelt stories or fun facts about their<br/> unique connection to the couple, making their role<br/> in the celebration even more meaningful!
+                                <p className='Roboto text-DimGray text-lg max-sm:text-sm leading-twentyseven tracking-0.5'>Showcase the bridal party and groomsmen with photos and<br /> brief bios. Share heartfelt stories or fun facts about their<br /> unique connection to the couple, making their role<br /> in the celebration even more meaningful!
                                 </p>
                                 <button className='Jost bg-SoftBeigePink hover:bg-SlateBlack text-white px-fortyfive py-fifteen textr-lg leading-27'>Appointment</button>
                             </div>
@@ -140,12 +169,12 @@ const Memories = () => {
                         <Slider {...settings} className='pb-5'>
                             {
                                 cardData.map((data) => (
-                                    <div key={data.id} className='px-3'>
+                                    <Link href={data.route} key={data.id} className='px-3'>
                                         <div className='relative flex justify-center max-w-fourtwentytwo w-full p-fifteen bg-white rounded-md mb-10'>
                                             <Image src={data.image} alt='cardimage1' />
                                             <div className='absolute Jost text-SlateBlack hover:text-white text-twentytwo max-sm:text-lg max-sm:py-3 bottom-[-22px] max-w-[220px] max-sm:max-w-[180px] w-full bg-white hover:bg-SlateBlack text-center leading-sixty'>{data.name}</div>
                                         </div>
-                                    </div>
+                                    </Link>
                                 ))
                             }
                         </Slider>
